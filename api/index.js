f90ba9c928e704a1a8d6ca3fff859b05c3d9cb72
honkita <elitelulww@gmail.com>
2024-06-18 18:58:07 -0400

    revert

--- api/index.js
+++ api/index.js
@@ -10,22 +10,18 @@ app.get("/", async (req, res) => {
   // first call
   var result1 = await fetch("https://jsonplaceholder.typicode.com/users");
 
-  res.send(
+  res.send(`
     <svg height="500" width="200" viewBox="0 0 500 200">
-      <g>
+        <g>
+        
         <circle r="100" cx="100" cy="100" fill="blue" />
-        <circle
-          r="50"
-          cx="100"
-          cy="100"
-          fill="transparent"
-          stroke="tomato"
-          stroke-width="100"
-          stroke-dasharray="calc(35 * 3.142 * 100 / 100) 314.2  "
-        />
-      </g>
+        <circle r="50" cx="100" cy="100" fill="transparent"
+                stroke="tomato"
+                stroke-width="100"
+                stroke-dasharray="calc(35 * 3.142 * 100 / 100) 314.2  "/>
+        </g>
     </svg>
-  );
+  `);
 });
 
 app.listen(PORT, () => {
