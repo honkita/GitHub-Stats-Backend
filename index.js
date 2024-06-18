88e54e3155fa268b35f498fc7be3316144856873
honkita <elitelulww@gmail.com>
2024-06-18 18:41:56 -0400

    test

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
 
-app.use(express.json()); // <==== parse request body as JSON
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-var resultJson1;
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.get("/api", async (req, res) => {
-  // first call
-  var result1 = await fetch("https://jsonplaceholder.typicode.com/users");
-
-  res.send(`
-    <svg height="500" width="200" viewBox="0 0 500 200">
-        <g>
-        
-        <circle r="100" cx="100" cy="100" fill="blue" />
-        <circle r="50" cx="100" cy="100" fill="transparent"
-                stroke="tomato"
-                stroke-width="100"
-                stroke-dasharray="calc(35 * 3.142 * 100 / 100) 314.2  "/>
-        </g>
-    </svg>
-  `);
-});
-
-app.listen(PORT, () => {
-  console.log("Server started");
-});
+module.exports = app;
