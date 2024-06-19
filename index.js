b3b8fcf62f7f08bb2b274d5b4ab0bbc0660bbb53
honkita <elitelulww@gmail.com>
2024-06-19 10:53:10 -0400

    TY BESTIE JAMES

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const PORT = 4000;
 app.use(express.json()); // <==== parse request body as JSON
 
 app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "image/svg+xml");
   res.send(`
-    <svg height="500" width="200" viewBox="0 0 500 200">
+    <svg xmlns="http://www.w3.org/2000/svg" height="500" width="200" viewBox="0 0 500 200">
         <g>
         
         <circle r="100" cx="100" cy="100" fill="blue" />
