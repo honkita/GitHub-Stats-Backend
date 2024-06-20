7e1841cf93132514a093c894574b2dfb7d619d0c
honkita <elitelulww@gmail.com>
2024-06-20 13:43:02 -0400

    generates graph

---
+++ api/GitHub.js
@@ -0,0 +1,57 @@
+const colours = require("../Assets/colours.json");
+module.exports = {
+  parseLink: async function (user, x, y, r) {
+    const p = await fetch("https://api.github.com/users/" + user + "/repos");
+    const request = await p.json();
+    const lan = {};
+    var t = 0;
+    for (let i = 0; i < request.length; i++) {
+      //console.log(this.posts[i]["name"]);
+      const pp = await fetch(request[i]["languages_url"]);
+      const ppp = await pp.json();
+
+      Object.keys(ppp).forEach(function (key) {
+        if (!(key in Object.keys(lan))) {
+          lan[key] = ppp[key];
+        } else {
+          lan[key] = lan[key] + ppp[key];
+        }
+      });
+    }
+
+    const sortedDict = Object.fromEntries(
+      Object.entries(lan).sort(([, a], [, b]) => a - b)
+    );
+    console.log(sortedDict);
+    var total = 0;
+    var keys = Object.keys(sortedDict);
+    keys.forEach(function (key) {
+      total = total + sortedDict[key];
+    });
+
+    console.log(total);
+    let sum = 0;
+
+    const c = colours["default"];
+    const styles = [];
+    //console.log(colours["default"]);
+    if (keys != null && total !== null && sortedDict !== null) {
+      for (let i = 0; i < keys.length; i++) {
+        const portion = sortedDict[keys[i]] / total;
+        const circumference = 2 * r * 3.14;
+        const p = portion * circumference;
+        const angle = portion * 360;
+        styles.push(
+          `<circle r="${r}" cx="${x}" cy="${y}" fill="transparent"
+                stroke="${c[i]}"
+                stroke-width="${r * 2}"
+                stroke-dasharray="${p} ${circumference}"
+                transform="rotate(${sum - 90} ${x} ${y})"/>
+            `
+        );
+        sum = sum + angle;
+      }
+      return styles.join("\r\n");
+    }
+  },
+};
--- api/index.js
+++ api/index.js
@@ -1,27 +1,64 @@
 const express = require("express");
+var githubParser = require("./GitHub");
 const app = express();
 const PORT = 4000;
 
+const box = { height: 500, width: 800, curve: 20, border: 10 };
+const circle = { x: box.width / 4, y: box.height / 2, r: 50 };
+
 app.use(express.json()); // <==== parse request body as JSON
+// console.log("FUCK");
+// let userToken = githubParser.parseLink("honkita");
+// userToken.then(function (result) {
+//   console.log(result); // "Some User token"
+// });
 
 app.get("/", (req, res) => {
+  //res.setHeader("Content-Type", "application/json");
   res.setHeader("Content-Type", "image/svg+xml");
-  res.send(`
-    <svg xmlns="http://www.w3.org/2000/svg" height="500" width="200" viewBox="0 0 500 200">
+  var link;
+  if (req.query.github != null) {
+    link = githubParser.parseLink(
+      req.query.github,
+      circle.x,
+      circle.y,
+      circle.r
+    );
+
+    //console.log(link);
+  }
+
+  link.then(function (result) {
+    link = result; // "Some User token"
+    //console.log(link);
+    res.send(
+      `<svg xmlns="http://www.w3.org/2000/svg" height="${box.height}" width="${box.width}" viewBox="0 0 1000 1000">
         <g>
-        
-        <circle r="100" cx="100" cy="100" fill="blue" />
-        <circle r="50" cx="100" cy="100" fill="transparent"
-                stroke="tomato"
-                stroke-width="100"
-                stroke-dasharray="calc(35 * 3.142 * 100 / 100) 314.2  "/>
+          <rect width="${box.width}" height="${box.height}" x="0" y="0" rx="${box.curve}" ry="${box.curve}" 
+            style="fill:transparent;stroke-width:${box.border};stroke:black" /> 
+          ${link}
         </g>
-    </svg>
-  `);
+                
+        
+      </svg>`
+    );
+  });
+
+  // res.setHeader("Content-Type", "image/svg+xml");
+  // res.send(`
+  //   <svg xmlns="http://www.w3.org/2000/svg" height="500" width="200" viewBox="0 0 500 200">
+  //       <g>
+
+  //       <circle r="100" cx="100" cy="100" fill="blue" />
+  //       <circle r="50" cx="100" cy="100" fill="transparent"
+  //               stroke="tomato"
+  //               stroke-width="100"
+  //               stroke-dasharray="calc(35 * 3.142 * 100 / 100) 314.2  "/>
+  //       </g>
+  //   </svg>
+  // `);
 });
 
-app.listen(PORT, () => {
-  console.log("Server started");
-});
+app.listen(PORT, () => {});
 
 module.exports = app;
