39ff253f85167212fb0a60fc61428cc97682f2c8
honkita <elitelulww@gmail.com>
2024-06-20 21:03:39 -0400

    change

--- api/index.js
+++ api/index.js
@@ -12,8 +12,6 @@ app.use(express.json()); // <==== parse request body as JSON
 // userToken.then(function (result) {
 //   console.log(result); // "Some User token"
 // });
-//
-
 app.get("/", (req, res) => {
   //res.setHeader("Content-Type", "application/json");
   res.setHeader("Content-Type", "image/svg+xml");
@@ -65,20 +63,6 @@ app.get("/", (req, res) => {
       </svg>`
     );
   });
-
-  // res.setHeader("Content-Type", "image/svg+xml");
-  // res.send(`
-  //   <svg xmlns="http://www.w3.org/2000/svg" height="500" width="200" viewBox="0 0 500 200">
-  //       <g>
-
-  //       <circle r="100" cx="100" cy="100" fill="blue" />
-  //       <circle r="50" cx="100" cy="100" fill="transparent"
-  //               stroke="tomato"
-  //               stroke-width="100"
-  //               stroke-dasharray="calc(35 * 3.142 * 100 / 100) 314.2  "/>
-  //       </g>
-  //   </svg>
-  // `);
 });
 
 app.listen(PORT, () => {});
